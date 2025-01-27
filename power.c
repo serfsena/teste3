b607b94eaec87394c776f7a09853203be15ff8b7
serfsena <2224399@aluno.univesp.br>
2025-01-26 22:59:37 -0300

    Alteração testando o Copilot.

--- power.c
+++ power.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 int main()
 {
-    for (int i = 1 ; i< 120; i++)
+    // numero de iterações de i*i que chegue próximo a 1000000
+    // Qual o valor de i*i que chega mais próximo de 1000000? 
+
+    for (int i = 1 ; i< 1000; i++)
     {
-        if(i*i>100000){
+        if(i*i > 1000000){
             break;
         }
         else{
-            printf("%d","%c"\n,(i*i)," ", i);
+            printf("%d%c%d\n",(i*i),' ',i);
         }
     }
 }
